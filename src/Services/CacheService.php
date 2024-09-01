3d5f237e2ff53c48863b6a8d1bf92881a220a37d
Ernest Uwadiegwu <ernestuwadiegwu@gmail.com>
2024-09-01 14:40:25 +0100

    Cache key to lowercase

--- src/Services/CacheService.php
+++ src/Services/CacheService.php
@@ -61,6 +61,6 @@ final readonly class CacheService implements CacheInterface
 
     private function cacheKey(string $cacheKey): string
     {
-        return $this->prefix . "." . $cacheKey;
+        return mb_strtolower($this->prefix . "." . $cacheKey);
     }
 }
